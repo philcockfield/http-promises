59543030a4e20bb2bcad499038438933b91fa9ab
Phil Cockfield <phil@cockfield.net>
2015-07-25 12:51:47 +1200

    Change path for entry modules to /lib

--- browser.js
+++ browser.js
@@ -1,3 +1,1 @@
-// Note: Register [babel] only if another module hasn't already done so.
-if (!global._babelPolyfill) { require('babel/register'); }
-module.exports = require('./src/http-browser');
+module.exports = require('./lib/http-browser');
